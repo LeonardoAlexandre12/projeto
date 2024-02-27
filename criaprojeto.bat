137f76972d50c43eeb61a8813e9e6573cf7a26d7
LeonardoAlexandre12 <160555061+LeonardoAlexandre12@users.noreply.github.com>
2024-02-26 22:15:19 -0300

    Add files via upload

---
+++ criaprojeto.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+d:
+md Sirene && cd Sirene && md audio css imagens js videos
+cd audio && type nul > audio.mp3 && cd..
+cd css && type nul > main.css && cd..
+cd imagens && type nul > foto.webp && cd..
+cd js && type nul > script.js && cd..
+cd videos && type nul > video.mkv && cd..
+type nul > index.html
+cd..
+code -r Sirene
